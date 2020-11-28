e23f87851dfabf3b728ba57d3774c3f057bdcd90
Yousif Abozid <yousif.abozid@yahoo.com>
2020-11-29 01:50:38 +0200

    connect to MongoDB

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import mongoose from 'mongoose'
 import productsRoutes from './routes/products.js'
 import homeRoutes from './routes/home.js'
 
 dotenv.config() // to access environment variables from .env file
 const app = express() // the actual express app
 app.use(cors()) // to allow the frontend to access the backend
+
+// connecting to MongoDB
+mongoose.connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false 
+    })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.log(`Can't connect to MongoDB => ${error.message}`))
 
 // routes
 app.use('/', homeRoutes)
