947ad95df2978a89c0c70c5b03adb114c481df73
Yousif Abozid <yousif.abozid@yahoo.com>
2021-01-21 19:00:09 +0200

    add delete product router and controller

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,14 @@
+import jwt from "jsonwebtoken"
+import asyncHandler from "express-async-handler"
+import { getTokenFrom } from "../utils/auth.js"
 import Product from "../models/product.js"
+import User from "../models/user.js"
 
 export const getProducts = (request, response) => {
     Product.find({})
         .then((data) => response.json(data))
         .catch(() =>
-            response.status(404).json({ message: "products not found" })
+            response.status(404).json({ message: "Products not found" })
         )
 }
 
@@ -12,6 +16,34 @@ export const getProduct = (request, response) => {
     Product.findById(request.params.id)
         .then((data) => response.json(data))
         .catch(() =>
-            response.status(404).json({ message: "product not found" })
+            response.status(404).json({ message: "Product not found" })
         )
 }
+
+export const deleteProduct = asyncHandler(async (request, response) => {
+    // first extract the token from the request headers
+    const token = getTokenFrom(request)
+
+    // then decode the token to know the user id
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+    // if the token is missing or invalid the code will stop exucting in the line above
+    // and this generating an error, and errorHandler will respond with appropriate status code and error message
+
+    // search for the user
+    const user = await User.findById(decodedToken.id)
+
+    // checks if the user is not an admin
+    if (!user.isAdmin) {
+        response
+            .status(401)
+            .json({ error: "Unauthorized, you are not an admin" })
+    } else {
+        const product = await Product.findById(request.params.id)
+        if (product) {
+            await product.remove()
+            response.json({ message: "Product Removed" })
+        } else {
+            response.status(404).json({ message: "Product not found" })
+        }
+    }
+})
--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,14 @@
 import express from "express"
-import { getProducts, getProduct } from "../controllers/products.js"
+import {
+    getProducts,
+    getProduct,
+    deleteProduct,
+} from "../controllers/products.js"
 
 const router = express.Router()
 
 router.get("/", getProducts)
 router.get("/:id", getProduct)
+router.delete("/:id", deleteProduct)
 
 export default router
